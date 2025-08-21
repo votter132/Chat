79ef019738392720870f19f33b02e2dd15d2bf20
liuyun <792045515@qq.com>
2025-08-21 17:22:45 +0800

    修改数据库

---
+++ server/prisma/migrations/20250821092018_/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX `Message_receiverId_fkey` ON `message`;
+
+-- DropIndex
+DROP INDEX `Message_senderId_fkey` ON `message`;
+
+-- AlterTable
+ALTER TABLE `message` ADD COLUMN `isRead` BOOLEAN NOT NULL DEFAULT false;
+
+-- AddForeignKey
+ALTER TABLE `Message` ADD CONSTRAINT `Message_senderId_fkey` FOREIGN KEY (`senderId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Message` ADD CONSTRAINT `Message_receiverId_fkey` FOREIGN KEY (`receiverId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
